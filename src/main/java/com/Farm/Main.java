692efc0c78293821c38c0ff6b34e7153fb7ee0f5
jamesdeards <62237784+jamesdeards@users.noreply.github.com>
2020-04-02 15:45:40 +0100

    AN-1

--- src/main/java/com/Farm/Main.java
+++ src/main/java/com/Farm/Main.java
@@ -8,17 +8,37 @@ import java.util.Scanner;
 
 public class Main extends Player {
 
+    public void saveToDatbase (Player player){
+
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
+
+        try {
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+            entityManager.persist(player);
+            entityManager.getTransaction().commit();
+            entityManagerFactory.close();
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
     public Animals randomAnimal() {
         return Animals.values()[new Random().nextInt(Animals.values().length)];
     }
 
     public Player createPlayer() throws PlayerException {
-        
+
         System.out.println("Welcome to the farm");
         System.out.println("Please enter your player name");
 
         Scanner scanner = new Scanner(System.in);
         String playerName = scanner.next();
+
         this.setPlayerName(playerName);
 
         return new Player(this.getPlayerName(),(20), randomAnimal());
@@ -27,28 +47,9 @@ public class Main extends Player {
 
     public static void main(String[] args) throws PlayerException {
 
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
-
-        Player player1 = new Player();
-        player1.setId(2);
-        player1.setPlayerName("James");
-        player1.setPlayerMoney(10);
-        player1.setPlayerAnimals(Animals.Cow);
-
-        Player player2 = new Player();
-        player2.setId(4);
-        player2.setPlayerName("Deards");
-        player2.setPlayerMoney(50);
-        player2.setPlayerAnimals(Animals.Horse);
-
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.merge(player1);
-        entityManager.merge(player2);
-        entityManager.getTransaction().commit();
-
-        entityManagerFactory.close();
+        Main main = new Main();
+        Player player1 = main.createPlayer();
+        main.saveToDatbase(player1);
 
     }
 }
