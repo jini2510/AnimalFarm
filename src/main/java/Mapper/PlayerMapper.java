6910d991915b3093591534d8317cf376c54edd23
jamesdeards <62237784+jamesdeards@users.noreply.github.com>
2020-04-02 10:55:57 +0100

    AN-1

---
+++ src/main/java/Mapper/PlayerMapper.java
@@ -0,0 +1,20 @@
+package Mapper;
+import com.Farm.Player;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PlayerMapper implements RowMapper<Player> {
+
+    @Override
+
+    public Player mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Player player = new Player();
+        player.setPlayerName(rs.getString("name"));
+        player.setPlayerMoney(rs.getInt("money"));
+
+        return player;
+    }
+}
