22f897c3c717d1344f7b1d5a8bb676799bcdc6dd
jamesdeards <62237784+jamesdeards@users.noreply.github.com>
2020-04-02 10:51:19 +0100

    AN-1

--- src/main/java/com/Farm/Animal.java
+++ src/main/java/com/Farm/Animal.java
@@ -14,6 +14,22 @@ public class Animal {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public int GetPrice() {
         return this.price;
     }
